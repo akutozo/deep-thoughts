b8214fb7e63d570216b54485e61d5222b92131ba
Jacob Daughtry <akutozo@gmail.com>
2020-10-24 15:40:04 -0500

    Updated root

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 
+//Provides data to other components
+import { ApolloProvider } from '@apollo/react-hooks';
+//Gets the data when we're ready to use it
+import ApolloClient from 'apollo-boost';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// establish a new connection to the GraphQL server using Apollo
+const client = new ApolloClient({
+  uri: '/graphql'
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,8 +6,6 @@ const expiration = '2h';
 module.exports = {
   signToken: function({ username, email, _id }) {
 
-    
-
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
