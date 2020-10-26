29b3a6a42a71e50ae191d20cfba25b3bbc9d0ae2
Jacob Daughtry <akutozo@gmail.com>
2020-10-25 23:17:39 -0500

    21.6.4 End

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 
 import ThoughtList from '../components/ThoughtList';
-
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import ThoughtForm from '../components/ThoughtForm';
 import FriendList from '../components/FriendList';
-import Auth from '../utils/auth';
+import { ADD_FRIEND } from '../utils/mutations';
 
 const Profile = () => {
   const { username: userParam } = useParams();
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
@@ -31,13 +33,29 @@ const Profile = () => {
       </h4>
     );
   }
-  
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
-      <h2 className="bg-dark text-secondary p-3 display-inline-block">
-        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-      </h2>
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -53,6 +71,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
